447c9cad69d5b415d1588123b1c95400ffcad4a1
bailiu <bailiu@microsoft.com>
2020-04-24 12:01:32 -0700

    Upload ServiceBus ConnectionFactory

---
+++ src/main/java/com/microsoft/azure/servicebus/jms/ServiceBusJmsTopicConnectionFactory.java
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package com.microsoft.azure.servicebus.jms;
+
+import javax.jms.JMSException;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
+
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
+
+public class ServiceBusJmsTopicConnectionFactory extends ServiceBusJmsConnectionFactory implements TopicConnectionFactory {
+
+    public ServiceBusJmsTopicConnectionFactory(String connectionString, ServiceBusJmsConnectionFactorySettings settings) {
+        super(connectionString, settings);
+    }
+    
+    public ServiceBusJmsTopicConnectionFactory(ConnectionStringBuilder connectionStringBuilder, ServiceBusJmsConnectionFactorySettings settings) {
+        super(connectionStringBuilder, settings);
+    }
+    
+    public ServiceBusJmsTopicConnectionFactory(String sasKeyName, String sasKey, String host, ServiceBusJmsConnectionFactorySettings settings) {
+        super(sasKeyName, sasKey, host, settings);
+    }
+
+    public TopicConnection createTopicConnection() throws JMSException {
+        return this.getConectionFactory().createTopicConnection();
+    }
+
+    public TopicConnection createTopicConnection(String userName, String password) throws JMSException {
+        return this.getConectionFactory().createTopicConnection(userName, password);
+    }
+}
