8ef92e30493bdc918086452b84b0d173fbf7efad
bailiu <bailiu@microsoft.com>
2020-06-03 09:47:29 -0700

    Let ServiceBusJmsConnectionFactory implement QueueConnectionFactory and TopicConnectionFactory as well

--- src/main/java/com/microsoft/azure/servicebus/jms/ServiceBusJmsConnectionFactory.java
+++ src/main/java/com/microsoft/azure/servicebus/jms/ServiceBusJmsConnectionFactory.java
@@ -10,13 +10,17 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSContext;
 import javax.jms.JMSException;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.TopicConnection;
+import javax.jms.TopicConnectionFactory;
 
 import org.apache.qpid.jms.JmsConnectionExtensions;
 import org.apache.qpid.jms.JmsConnectionFactory;
 
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 
-public class ServiceBusJmsConnectionFactory implements ConnectionFactory {
+public class ServiceBusJmsConnectionFactory implements ConnectionFactory, QueueConnectionFactory, TopicConnectionFactory {
     private final JmsConnectionFactory factory;
     private ConnectionStringBuilder builder;
     
@@ -54,27 +58,53 @@ public class ServiceBusJmsConnectionFactory implements ConnectionFactory {
         return builder;
     }
     
+    @Override
     public Connection createConnection() throws JMSException {
         return this.factory.createConnection();
     }
 
+    @Override
     public Connection createConnection(String userName, String password) throws JMSException {
         return this.factory.createConnection(userName, password);
     }
 
+    @Override
     public JMSContext createContext() {
         return this.factory.createContext();
     }
 
+    @Override
     public JMSContext createContext(int sessionMode) {
         return this.factory.createContext(sessionMode);
     }
 
+    @Override
     public JMSContext createContext(String userName, String password) {
         return this.factory.createContext(userName, password);
     }
 
+    @Override
     public JMSContext createContext(String userName, String password, int sessionMode) {
         return this.factory.createContext(userName, password, sessionMode);
+    }
+
+    @Override
+    public TopicConnection createTopicConnection() throws JMSException {
+        return this.factory.createTopicConnection();
+    }
+
+    @Override
+    public TopicConnection createTopicConnection(String userName, String password) throws JMSException {
+        return this.factory.createTopicConnection(userName, password);
+    }
+
+    @Override
+    public QueueConnection createQueueConnection() throws JMSException {
+        return this.factory.createQueueConnection();
+    }
+
+    @Override
+    public QueueConnection createQueueConnection(String userName, String password) throws JMSException {
+        return this.factory.createQueueConnection(userName, password);
     }
 }
