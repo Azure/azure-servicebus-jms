4cfd6627cd0e87523385b0c2573e701f9f3d3598
Karen Alfaro Salazar <kaalfaro@microsoft.com>
2023-01-24 21:58:47 -0800

    End to end test testing service side creation from portal and sdk.

---
+++ src/test/java/com/microsoft/azure/servicebus/jms/aad/QueueTest.java
@@ -0,0 +1,181 @@
+package com.microsoft.azure.servicebus.jms.aad;
+
+import org.junit.Test;
+
+public class QueueTest {
+	TestInitialization initialization;
+	
+	@Test
+	public void e2EQueueTestWithSecretCredential()throws Exception {
+		System.out.println("\nTest send and received queue on the app using SecretCredential");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("endToEndSecretCredentialQueue");
+		
+		this.initialization.startAad("Queue", this.initialization.GetSecretCredential());
+		this.initialization.entitiy = initialization.jmsContext.createQueue(initialization.entityName);
+        System.out.println("Queue was created.......");
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entitiy);
+	    System.out.println("Consumer was created.....");
+	    
+	    //send message
+	    try {
+	    	this.initialization.send(10);
+	    	this.initialization.received(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}
+	
+	@Test
+	public void e2EQueueTestWithMsiCredential()throws Exception {
+		System.out.println("\nTest send and received queue on the app using MsiCredential");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("endToEndMsiQueue");
+		
+		this.initialization.startAad("Queue", this.initialization.GetMsiCredential());
+		this.initialization.entitiy = initialization.jmsContext.createQueue(initialization.entityName);
+        System.out.println("Queue was created.......");
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entitiy);
+	    System.out.println("Consumer was created.....");
+	    
+	    //send message
+	    try {
+	    	this.initialization.send(10);
+	    	this.initialization.received(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}
+	
+	@Test
+	public void e2EQueueTestWithDefaultAzureCredential()throws Exception {
+		System.out.println("\nTest send and received queue on the app using DefaultAzureCredential");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("endToEndDefaultAzureCredentialQueue");
+		
+		this.initialization.startAad("Queue", null);
+		this.initialization.entitiy = initialization.jmsContext.createQueue(initialization.entityName);
+        System.out.println("Queue was created.......");
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entitiy);
+	    System.out.println("Consumer was created.....");
+
+	    try {
+	    	this.initialization.send(10);
+	    	this.initialization.received(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}
+	
+	/*
+	 * PORTAL
+	 * This test assume entity exist on portal
+	 */
+	
+	@Test
+	public void portalE2EQueueTestWithSecret()throws Exception {
+		System.out.println("\nTest send and received queue portal test using SecretCredential");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("PortalSecretQueue");
+		
+		this.initialization.startAad("Queue", this.initialization.GetSecretCredential());
+		this.initialization.entitiy = initialization.jmsContext.createQueue(initialization.entityName);
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entitiy);
+	    System.out.println("Consumer was created.....");
+	    
+	    //send message
+	    try {
+	    	this.initialization.send(10);
+	    	this.initialization.received(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}
+	
+	@Test
+	public void portalE2EQueueTestWithMsi()throws Exception {
+		System.out.println("\nTest send and received queue portal test using MsiCredential");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("PortalMsiQueue");
+		
+		this.initialization.startAad("Queue", this.initialization.GetMsiCredential());
+		this.initialization.entitiy = initialization.jmsContext.createQueue(initialization.entityName);
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entitiy);
+	    System.out.println("Consumer was created.....");
+	    
+	    //send message
+	    try {
+	    	this.initialization.send(10);
+	    	this.initialization.received(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}
+	
+	@Test
+	public void portalE2EQueueTestWithDefaultAzure()throws Exception {
+		System.out.println("\nTest send and received queue portal test using DefaultAzureCredential");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("PortalDefaultAzureQueue");
+		
+		this.initialization.startAad("Queue", null);
+		this.initialization.entitiy = initialization.jmsContext.createQueue(initialization.entityName);
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entitiy);
+	    System.out.println("Consumer was created.....");
+
+	    try {
+	    	this.initialization.send(10);
+	    	this.initialization.received(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}
+}
