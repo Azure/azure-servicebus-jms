9f82dcbe1fe14e03a6753b10c6a1a22106113ea2
Shankar Reddy Sama <shsama@microsoft.com>
2023-03-07 16:01:14 -0800

    Adding aad authentication support for jms
    Notes:
        Merge pull request #28 from ksalazar-91/AadEnableServiceBusJms
    
        Adding aad authentication support for jms
    
    commit 868ad3bf4126ffa3f0064eceee74f4fb219df495
    Author: Karen Alfaro Salazar <kaalfaro@microsoft.com>
    Date:   Tue Jan 31 12:58:51 2023 -0800

---
+++ src/test/java/com/microsoft/azure/servicebus/jms/connection/string/QueueTests.java
@@ -0,0 +1,92 @@
+// Copyright (c) Microsoft. All rights reserved.
+package com.microsoft.azure.servicebus.jms.connection.string;
+
+import javax.jms.TextMessage;
+import org.junit.Test;
+import com.microsoft.azure.servicebus.jms.aad.TestInitialization;
+
+public class QueueTests {	
+    TextMessage msg;
+    
+	TestInitialization initialization;
+	
+	@Test
+	public void sendToQueueOnly()throws Exception {
+		System.out.println("\nTest send and creating queue on the app using connection string");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("sendOnlyCs");
+		this.initialization.initializeWithSas("Queue");
+		this.initialization.entity = initialization.jmsContext.createQueue(initialization.entityName);
+        System.out.println("Queue was created.......");
+        
+        this.initialization.producer = initialization.jmsContext.createProducer();
+        System.out.println("Producer was created.....");
+
+	    try {
+	    	this.initialization.send(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    	
+	    }		
+	}
+	
+	@Test
+	public void receivedToQueueOnly()throws Exception {
+		System.out.println("\nTest send and creating queue on the app using connection string");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("sendOnlyCs");
+		this.initialization.initializeWithSas("Queue");
+		this.initialization.entity = initialization.jmsContext.createQueue(initialization.entityName);
+        System.out.println("Queue was created.......");
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entity);
+	    System.out.println("Consumer was created.....");
+	    
+	    try {
+	    	this.initialization.received(10);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}
+	
+	@Test
+	public void endToEndQueueTest()throws Exception {
+		System.out.println("\nTest send and creating queue on the app using connection string");
+		System.out.println("---------------------------------------------------------");
+		this.initialization = new TestInitialization("sendOnlyCs");
+		this.initialization.initializeWithSas("Queue");
+		this.initialization.entity = initialization.jmsContext.createQueue(initialization.entityName);
+        System.out.println("Queue was created.......");
+        
+		this.initialization.consumer = initialization.jmsContext.createConsumer(initialization.entity);
+	    System.out.println("Consumer was created.....");
+	    
+	    //send message
+	    try {
+	    	this.initialization.send(10);
+	    	this.initialization.received(0);
+	    }catch (Exception e) {
+	    	e.printStackTrace();
+	    	throw new Exception(e);
+	    }finally {
+	    	this.initialization.jmsContext.close();
+	    	this.initialization.consumer.close();
+	    	System.out.println("---------------------------------------------------------");
+		    System.out.println("End Test");
+		    System.out.println("=========================================================");
+	    }	    
+	}	
+}
